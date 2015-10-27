c25a00dd85bc6693f7b399120c3bcf4ac08dae4d
Chaotian Jing <chaotian.jing@mediatek.com>
2015-10-27 14:24:21 +0800

    mmc: core: Add DT bindings for eMMC hardware reset support
    Sometime only need set MMC_CAP_HW_RESET for one of MMC hosts,
    So set it in device tree is better.
    
    Conflicts:
    	drivers/mmc/core/host.c
    
    Change-Id: I13b05e3471ca7b19ee3f8e9f3ed558a7fab9b052
    Signed-off-by: Chaotian Jing <chaotian.jing@mediatek.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Sowon Na <sowon.na@samsung.com>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -271,6 +271,8 @@ int mmc_of_parse(struct mmc_host *host)
 		host->caps |= MMC_CAP_HW_RESET;
 	if (of_property_read_bool(np, "cap-sdio-irq"))
 		host->caps |= MMC_CAP_SDIO_IRQ;
+	if (of_property_read_bool(np, "cap-mmc-hw-reset"))
+		host->caps |= MMC_CAP_HW_RESET;
 	if (of_property_read_bool(np, "full-pwr-cycle"))
 		host->caps2 |= MMC_CAP2_FULL_PWR_CYCLE;
 	if (of_property_read_bool(np, "keep-power-in-suspend"))
