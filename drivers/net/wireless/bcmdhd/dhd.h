f4a4563abd634ec69bc91c68f70ef1474bf15c4d
Patrick Tjin <pattjin@google.com>
2015-12-09 21:17:56 -0800

    net: wireless: bcmdhd: check packet length for event messages
    Check the datalen field is less than the size of
    packet received from the network.
    
    Signed-off-by: Patrick Tjin <pattjin@google.com>
    
    Bug: 27335472
    
    [fixes CVE-2016-0802]
    Change-Id: I5f674d21a48ae9610db343e724c4c59c49f01457
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- drivers/net/wireless/bcmdhd/dhd.h
+++ drivers/net/wireless/bcmdhd/dhd.h
@@ -1143,7 +1143,7 @@ extern struct net_device * dhd_idx2net(void *pub, int ifidx);
 extern int net_os_send_hang_message(struct net_device *dev);
 extern bool dhd_wowl_cap(void *bus);
 
-extern int wl_host_event(dhd_pub_t *dhd_pub, int *idx, void *pktdata,
+extern int wl_host_event(dhd_pub_t *dhd_pub, int *idx, void *pktdata, size_t pktlen,
                          wl_event_msg_t *, void **data_ptr,  void *);
 extern void wl_event_to_host_order(wl_event_msg_t * evt);
 extern int wl_host_event_get_data(void *pktdata, wl_event_msg_t *event, void **data_ptr);
