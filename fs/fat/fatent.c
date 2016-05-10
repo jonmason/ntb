f4c0815e92fe7f61c04dc5913a65760ae0447257
Sowon Na <sowon.na@samsung.com>
2016-05-10 13:39:39 +0900

    fat: change log level to avoid log flooding
    This patch is a temporal patch to avoid log flooding
    when unmount fat file system suddenly.
    
    FAT-fs (mmcblk1p1): FAT read failed (blocknr 1)
    
    Change-Id: I95944e8bd0e43049f0199547d8b6be4113372639
    Signed-off-by: Sowon Na <sowon.na@samsung.com>

--- fs/fat/fatent.c
+++ fs/fat/fatent.c
@@ -105,7 +105,7 @@ static int fat_ent_bread(struct super_block *sb, struct fat_entry *fatent,
 	fatent->fat_inode = MSDOS_SB(sb)->fat_inode;
 	fatent->bhs[0] = sb_bread(sb, blocknr);
 	if (!fatent->bhs[0]) {
-		fat_msg(sb, KERN_ERR, "FAT read failed (blocknr %llu)",
+		fat_msg(sb, KERN_NOTICE, "FAT read failed (blocknr %llu)",
 		       (llu)blocknr);
 		return -EIO;
 	}
