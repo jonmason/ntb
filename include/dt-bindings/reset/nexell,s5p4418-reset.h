7a627441ea3a82d15b95b7e028536a465d7f800c
Bon-gyu, KOO <freestyle@nexell.co.kr>
2016-05-13 19:53:54 +0900

    reset: nexell: add reset for s5p4418 SoC
    This patch provides reset for s5p4418 SoC.
    s5p4418 soc is same as s5p6818, so use same driver.
    
    Change-Id: I70713d74c9bbdef08838181a7a1a2268bf1a16c1
    Signed-off-by: Bon-gyu, KOO <freestyle@nexell.co.kr>

---
+++ include/dt-bindings/reset/nexell,s5p4418-reset.h
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2016  Nexell Co., Ltd.
+ * Author: Bon-gyu, KOO <freestyle@nexell.co.kr>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Reset ID
+ */
+
+#define RESET_ID_START		0
+#define RESET_ID_AC97		0
+#define RESET_ID_CPU1		1
+#define RESET_ID_CPU2		2
+#define RESET_ID_CPU3		3
+#define RESET_ID_WD1		4
+#define RESET_ID_WD2		5
+#define RESET_ID_WD3		6
+#define RESET_ID_CRYPTO		7
+#define RESET_ID_DEINTERLACE	8
+#define RESET_ID_DISP_TOP	9
+#define RESET_ID_DISPLAY	10
+#define RESET_ID_RESCONV	11
+#define RESET_ID_LCDIF		12
+#define RESET_ID_HDMI		13
+#define RESET_ID_HDMI_VIDEO	14
+#define RESET_ID_HDMI_SPDIF	15
+#define RESET_ID_HDMI_TMDS	16
+#define RESET_ID_HDMI_PHY	17
+#define RESET_ID_LVDS		18
+#define RESET_ID_ECID		19
+#define RESET_ID_I2C0		20
+#define RESET_ID_I2C1		21
+#define RESET_ID_I2C2		22
+#define RESET_ID_I2S0		23
+#define RESET_ID_I2S1		24
+#define RESET_ID_I2S2		25
+#define RESET_ID_DREX_C		26
+#define RESET_ID_DREX_A		27
+#define RESET_ID_DREX		28
+#define RESET_ID_MIPI		29
+#define RESET_ID_MIPI_DSI	30
+#define RESET_ID_MIPI_CSI	31
+#define RESET_ID_MIPI_PHY_S	32
+#define RESET_ID_MIPI_PHY_M	33
+#define RESET_ID_MPEGTSI	34
+#define RESET_ID_PDM		35
+#define RESET_ID_TIMER		36
+#define RESET_ID_PWM		37
+#define RESET_ID_SCALER		38
+#define RESET_ID_SDMMC0		39
+#define RESET_ID_SDMMC1		40
+#define RESET_ID_SDMMC2		41
+#define RESET_ID_SPDIFRX	42
+#define RESET_ID_SPDIFTX	43
+#define RESET_ID_SSP0_P		44
+#define RESET_ID_SSP0		45
+#define RESET_ID_SSP1_P		46
+#define RESET_ID_SSP1		47
+#define RESET_ID_SSP2_P		48
+#define RESET_ID_SSP2		49
+#define RESET_ID_UART0		50
+#define RESET_ID_UART1		51
+#define RESET_ID_UART2		52
+#define RESET_ID_UART3		53
+#define RESET_ID_UART4		54
+#define RESET_ID_UART5		55
+#define RESET_ID_USB20HOST	56
+#define RESET_ID_USB20OTG	57
+#define RESET_ID_WDT		58
+#define RESET_ID_WDT_POR	59
+#define RESET_ID_ADC		60
+#define RESET_ID_CODA_A		61
+#define RESET_ID_CODA_P		62
+#define RESET_ID_CODA_C		63
+#define RESET_ID_DWC_GMAC	64
+#define RESET_ID_VR		65
+#define RESET_ID_PPM		66
+#define RESET_ID_VIP1		67
+#define RESET_ID_VIP0		68
+#define RESET_ID_END		(RESET_ID_VIP0)
