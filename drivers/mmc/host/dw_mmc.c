ba25e7dd54420eab138caa257ca97361d36d7c26
Soon-Yong Cho <soonyong.cho@samsung.com>
2016-07-13 22:38:37 +0900

    mmc: add "ignore mmc pm notify" functionality
    This patch addes mmc function to ignore mmc pm notify.
    original commit is below.
    
    commit 4c40080bb1d4f68000b7a4c397c1bc8a623668fa
    Author: Dmitry Shmidt <dimitrysh <at> android.com>
    Date:   Mon Oct 11 11:28:54 2010 -0700
    
        mmc: Add "ignore mmc pm notify" functionality
    
    Change-Id: Ic337f57e65f153f071fe85e197ede60d04e5f7ce
    Signed-off-by: Soon-Yong Cho <soonyong.cho@samsung.com>

--- drivers/mmc/host/dw_mmc.c
+++ drivers/mmc/host/dw_mmc.c
@@ -2591,8 +2591,10 @@ static int dw_mci_init_slot(struct dw_mci *host, unsigned int id)
 	if (host->pdata->caps)
 		mmc->caps = host->pdata->caps;
 
-	if (host->pdata->pm_caps)
+	if (host->pdata->pm_caps) {
 		mmc->pm_caps = host->pdata->pm_caps;
+		mmc->pm_flags = mmc->pm_caps;
+	}
 
 	if (host->dev->of_node) {
 		ctrl_id = of_alias_get_id(host->dev->of_node, "mshc");
@@ -2979,6 +2981,9 @@ static struct dw_mci_board *dw_mci_parse_dt(struct dw_mci *host)
 		dev_info(dev, "supports-highspeed property is deprecated.\n");
 		pdata->caps |= MMC_CAP_SD_HIGHSPEED | MMC_CAP_MMC_HIGHSPEED;
 	}
+
+	if (of_find_property(np, "pm-ignore-notify", NULL))
+		pdata->pm_caps |= MMC_PM_IGNORE_PM_NOTIFY;
 
 	if (of_find_property(np, "cd-type-external", NULL)) {
 		pdata->cd_type = DW_MCI_CD_EXTERNAL;
