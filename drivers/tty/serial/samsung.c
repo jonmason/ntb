67f241d635191a5dd6bf509b4b132ee9d6e4d2ba
Soon-Yong Cho <soonyong.cho@samsung.com>
2016-07-22 16:19:37 +0900

    serial: samsung: fix bt packet loss issue during sleep
    While sleeping procedure, UART nRTS could be inactivated.
    This patch disables AFC(auto flow control) and changes nRTS signal
    to High(Inactivate nRTS) while sleeping procedure.
    
    Change-Id: I070b9b9a4bfbe178c4bd616b9ce9fcd42b63c5dd
    Signed-off-by: Soon-Yong Cho <soonyong.cho@samsung.com>

--- drivers/tty/serial/samsung.c
+++ drivers/tty/serial/samsung.c
@@ -1078,6 +1078,7 @@ static void s3c24xx_serial_pm(struct uart_port *port, unsigned int level,
 {
 	struct s3c24xx_uart_port *ourport = to_ourport(port);
 	int timeout = 10000;
+	unsigned int umcon;
 
 	ourport->pm_level = level;
 
@@ -1085,6 +1086,10 @@ static void s3c24xx_serial_pm(struct uart_port *port, unsigned int level,
 	case 3:
 		while (--timeout && !s3c24xx_serial_txempty_nofifo(port))
 			udelay(100);
+
+		umcon = rd_regl(port, S3C2410_UMCON);
+		umcon &= ~(S3C2410_UMCOM_AFC | S3C2410_UMCOM_RTS_LOW);
+		wr_regl(port, S3C2410_UMCON, umcon);
 
 		if (!IS_ERR(ourport->baudclk))
 			clk_disable_unprepare(ourport->baudclk);
