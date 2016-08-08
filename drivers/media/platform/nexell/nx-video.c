e3a818d6e4c34da2e030b5c14ea303fc79c75c4c
Sungwoo Park <swpark@nexell.co.kr>
2016-08-08 15:08:08 +0900

    media: nexell: Remove WARN_ON when nx_video_get_next_buffer failed
    This patch removes WARN_ON in nx_video_done_buffer function when
    nx_video_get_next_buffer() failed. In some application like v4l2src
    gst-plugin, when exiting by CTRL+C, stream on/off code is repeated with
    no queuing buffer so, WARN_ON code spews verbose error message
    unintentionally.
    
    Change-Id: I3ab5ae781e039bdbfa0bd29d00df18f18eb37b86
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- drivers/media/platform/nexell/nx-video.c
+++ drivers/media/platform/nexell/nx-video.c
@@ -1321,10 +1321,8 @@ bool nx_video_done_buffer(struct nx_video_buffer_object *obj)
 	}
 
 	done_buf = nx_video_get_next_buffer(obj, true);
-	if (!done_buf) {
-		WARN_ON(1);
+	if (!done_buf)
 		return false;
-	}
 
 	if (done_buf->cb_buf_done) {
 		done_buf->consumer_index++;
