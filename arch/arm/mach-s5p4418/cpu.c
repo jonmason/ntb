994c78acc1687df85f4b996efc9640a66e3ee0d6
Youngbok Park <ybpark@nexell.co.kr>
2016-08-23 15:08:32 +0900

    arm: nexell: l2c: use write_sec function
    This patch add support write_sec function.
    When you use the psci kernel is non-secure mode.
    so add support write_sec function.'
    
    Change-Id: I13c6ce325d1aa70880ff9e171bfcb0c596c6b681
    Signed-off-by: Youngbok Park <ybpark@nexell.co.kr>

--- arch/arm/mach-s5p4418/cpu.c
+++ arch/arm/mach-s5p4418/cpu.c
@@ -20,6 +20,7 @@
 #include <linux/reboot.h>
 #include <asm/mach/arch.h>
 #include <linux/io.h>
+#include <linux/soc/nexell/sec_reg.h>
 
 #define PHYS_BASE_CLKPWR	0xC0010000
 #define ALIVE_GATE	0x800
@@ -28,6 +29,17 @@
 
 #define SW_RESET_EN	(1<<3)
 #define SW_RESET	(1<<12)
+
+
+#ifdef CONFIG_SECURE_REG_ACCESS
+
+#define S5P4418_L2C_BASE 0xcf000000
+
+void l2c_write_sec(unsigned long val, unsigned reg)
+{
+	write_sec_reg((void __iomem *) S5P4418_L2C_BASE + reg, val);
+}
+#endif
 
 void s5p4418_reboot(enum reboot_mode mode, const char *cmd)
 {
@@ -49,16 +61,19 @@ static void __init cpu_init_machine(void)
 static const char * const cpu_dt_compat[] = {
 	"nexell,s5p4418",
 	NULL
-	};
+};
 /*------------------------------------------------------------------------------
  * Maintainer: Nexell Co., Ltd.
  */
 
 DT_MACHINE_START(S5P4418, "s5p4418")
-	.l2c_aux_val	= 0x70470001,
-	.l2c_aux_mask	= 0xfc000fff,
+	.l2c_aux_val	= 0x7e470000,
+	.l2c_aux_mask	= 0x81b8ffff,
 	.init_machine	= cpu_init_machine,
 	.dt_compat	= cpu_dt_compat,
 	.restart	= s5p4418_reboot,
+#ifdef CONFIG_SECURE_REG_ACCESS
+	.l2c_write_sec	= l2c_write_sec,
+#endif
 MACHINE_END
 
