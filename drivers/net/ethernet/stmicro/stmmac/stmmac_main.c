jonmason/ntb

commit 39c6ea9831f7d6935545fffe7adcf3e08c1255d3

stmmac: do not sleep in atomic context for mdio_reset
stmmac_mdio_reset() has been updated to use msleep rather udelay
(as some PHY requires a one second delay there).
It called from stmmac_resume() within the spin_lock_irqsave block
atomic context triggering 'scheduling while atomic'.

The stmmac_priv lock usage is not fully documented, but it seems
to protect the access to the MAC registers / DMA structures rather
than the MDIO bus or the PHY (which have separate locking),
so we can push the spin_lock after the stmmac_mdio_reset call.

Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
Signed-off-by: David S. Miller <davem@davemloft.net>

drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -3087,8 +3087,6 @@ int stmmac_resume(struct net_device *ndev)
 	if (!netif_running(ndev))
 		return 0;
 
-	spin_lock_irqsave(&priv->lock, flags);
-
 	/* Power Down bit, into the PM register, is cleared
 	 * automatically as soon as a magic packet or a Wake-up frame
 	 * is received. Anyway, it's better to manually clear
@@ -3096,7 +3094,9 @@ int stmmac_resume(struct net_device *ndev)
 	 * from another devices (e.g. serial console).
 	 */
 	if (device_may_wakeup(priv->device)) {
+		spin_lock_irqsave(&priv->lock, flags);
 		priv->hw->mac->pmt(priv->hw, 0);
+		spin_unlock_irqrestore(&priv->lock, flags);
 		priv->irq_wake = 0;
 	} else {
 		pinctrl_pm_select_default_state(priv->device);
@@ -3109,6 +3109,8 @@ int stmmac_resume(struct net_device *ndev)
 	}
 
 	netif_device_attach(ndev);
+
+	spin_lock_irqsave(&priv->lock, flags);
 
 	priv->cur_rx = 0;
 	priv->dirty_rx = 0;

commit 9bb66aebdd431c4e6ece6e0d606ad6e33ab570c1

WIP: net: stmmac: expand spinlock period except mdio_reset
This patch is an workaround code for addressing lock-up issue
during suspend resume. It might be required more section to
protect by spinlock and roll back to the point before below
patch. (But, still required release the spinlock before calling
mdio_reset).

Fixes: 39c6ea9831f7 ("stmmac: do not sleep in atomic context for
mdio_reset")

Change-Id: If938375f209d85bed66a36dd822f106f016095a2
Signed-off-by: Chanho Park <chanho61.park@samsung.com>

drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -3087,6 +3087,8 @@ int stmmac_resume(struct net_device *ndev)
 	if (!netif_running(ndev))
 		return 0;
 
+	spin_lock_irqsave(&priv->lock, flags);
+
 	/* Power Down bit, into the PM register, is cleared
 	 * automatically as soon as a magic packet or a Wake-up frame
 	 * is received. Anyway, it's better to manually clear
@@ -3094,9 +3096,7 @@ int stmmac_resume(struct net_device *ndev)
 	 * from another devices (e.g. serial console).
 	 */
 	if (device_may_wakeup(priv->device)) {
-		spin_lock_irqsave(&priv->lock, flags);
 		priv->hw->mac->pmt(priv->hw, 0);
-		spin_unlock_irqrestore(&priv->lock, flags);
 		priv->irq_wake = 0;
 	} else {
 		pinctrl_pm_select_default_state(priv->device);
@@ -3104,13 +3104,14 @@ int stmmac_resume(struct net_device *ndev)
 		clk_enable(priv->stmmac_clk);
 		clk_enable(priv->pclk);
 		/* reset the phy so that it's ready */
-		if (priv->mii)
+		if (priv->mii) {
+			spin_unlock_irqrestore(&priv->lock, flags);
 			stmmac_mdio_reset(priv->mii);
+			spin_lock_irqsave(&priv->lock, flags);
+		}
 	}
 
 	netif_device_attach(ndev);
-
-	spin_lock_irqsave(&priv->lock, flags);
 
 	priv->cur_rx = 0;
 	priv->dirty_rx = 0;