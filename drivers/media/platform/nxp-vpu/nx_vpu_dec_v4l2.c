jonmason/ntb

commit 96fc733e5440a47e809a9038b71ee659a2a8d0ad

media: nexell: vpu: fix stack allocation
This patch corrects below warning during build. The size of
vpu_dec_reg_frame_arg is too big and it will need to allocate
from heap.

drivers/media/platform/nxp-vpu/nx_vpu_dec_v4l2.c:1000:1: warning: the
frame size of 4208 bytes is larger than 1024 bytes
[-Wframe-larger-than=]

Change-Id: I2647e502e3dce70b8b136f9d69c4eca9be9b2276
Signed-off-by: Chanho Park <chanho61.park@samsung.com>

drivers/media/platform/nxp-vpu/nx_vpu_dec_v4l2.c
@@ -945,7 +945,7 @@ int vpu_dec_parse_vid_cfg(struct nx_vpu_ctx *ctx)
 int vpu_dec_init(struct nx_vpu_ctx *ctx)
 {
 	struct vpu_dec_ctx *dec_ctx = &ctx->codec.dec;
-	struct vpu_dec_reg_frame_arg frameArg;
+	struct vpu_dec_reg_frame_arg *frameArg = dec_ctx->frameArg;
 	int i, ret = 0;
 
 	FUNC_IN();
@@ -955,9 +955,7 @@ int vpu_dec_init(struct nx_vpu_ctx *ctx)
 		return -EAGAIN;
 	}
 
-	NX_DrvMemset(&frameArg, 0, sizeof(frameArg));
-
-	frameArg.chromaInterleave = ctx->chromaInterleave;
+	frameArg->chromaInterleave = ctx->chromaInterleave;
 
 	if (ctx->codec_mode != CODEC_STD_MJPG) {
 		ret = alloc_decoder_memory(ctx);
@@ -967,31 +965,31 @@ int vpu_dec_init(struct nx_vpu_ctx *ctx)
 		}
 
 		if (dec_ctx->slice_buf)
-			frameArg.sliceBuffer = *dec_ctx->slice_buf;
+			frameArg->sliceBuffer = *dec_ctx->slice_buf;
 		if (dec_ctx->col_mv_buf)
-			frameArg.colMvBuffer = *dec_ctx->col_mv_buf;
+			frameArg->colMvBuffer = *dec_ctx->col_mv_buf;
 		if (dec_ctx->pv_slice_buf)
-			frameArg.pvbSliceBuffer = *dec_ctx->pv_slice_buf;
+			frameArg->pvbSliceBuffer = *dec_ctx->pv_slice_buf;
 
-		frameArg.sramAddr = ctx->dev->sram_base_addr;
-		frameArg.sramSize = ctx->dev->sram_size;
+		frameArg->sramAddr = ctx->dev->sram_base_addr;
+		frameArg->sramSize = ctx->dev->sram_size;
 	}
 
 	for (i = 0; i < dec_ctx->dpb_queue_cnt; i++) {
-		frameArg.frameBuffer[i].phyAddr[0]
+		frameArg->frameBuffer[i].phyAddr[0]
 			= dec_ctx->frame_buf[i].phyAddr[0];
-		frameArg.frameBuffer[i].phyAddr[1]
+		frameArg->frameBuffer[i].phyAddr[1]
 			= dec_ctx->frame_buf[i].phyAddr[1];
 
 		if (ctx->chromaInterleave == 0)
-			frameArg.frameBuffer[i].phyAddr[2]
+			frameArg->frameBuffer[i].phyAddr[2]
 				= dec_ctx->frame_buf[i].phyAddr[2];
 
-		frameArg.frameBuffer[i].stride[0] = ctx->buf_y_width;
+		frameArg->frameBuffer[i].stride[0] = ctx->buf_y_width;
 	}
-	frameArg.numFrameBuffer = dec_ctx->dpb_queue_cnt;
+	frameArg->numFrameBuffer = dec_ctx->dpb_queue_cnt;
 
-	ret = NX_VpuDecRegFrameBuf(ctx->hInst, &frameArg);
+	ret = NX_VpuDecRegFrameBuf(ctx->hInst, frameArg);
 	if (ret != VPU_RET_OK)
 		NX_ErrMsg(("NX_VpuDecRegFrameBuf() failed.(ErrorCode=%d)\n",
 			ret));
@@ -1282,6 +1280,11 @@ int alloc_decoder_memory(struct nx_vpu_ctx *ctx)
 		}
 	}
 
+	dec_ctx->frameArg = kzalloc(sizeof(struct vpu_dec_reg_frame_arg),
+			GFP_KERNEL);
+	if (!dec_ctx->frameArg)
+		goto Error_Exit;
+
 	return 0;
 
 Error_Exit:
@@ -1314,6 +1317,8 @@ int free_decoder_memory(struct nx_vpu_ctx *ctx)
 
 	if (ctx->bit_stream_buf)
 		nx_free_memory(ctx->bit_stream_buf);
+
+	kfree(dec_ctx->frameArg);
 
 	return 0;
 }

drivers/media/platform/nxp-vpu/nx_vpu_v4l2.h
@@ -127,6 +127,8 @@ struct vpu_dec_ctx {
 
 	/* for Jpeg */
 	int32_t thumbnailMode;
+
+	struct vpu_dec_reg_frame_arg *frameArg;
 };
 
 struct nx_vpu_fmt {

commit 9c0b4b404a77f1b48837233bedb2ded4b156bca8

media: nexell: vpu: fix null pointer bug
Fixes: 96fc733e5440 ("media: nexell: vpu: fix stack allocation")

This patch fixed null pointer dereferenced bug in 96fc733e5440a47e809a9038b71ee659a
2a8d0ad commit.

Change-Id: Ia31b4ad8489968ed90e365de63471c190529a61d
Signed-off-by: sunghwansoon <hs2704.sung@samsung.com>

drivers/media/platform/nxp-vpu/nx_vpu_dec_v4l2.c
@@ -945,7 +945,7 @@ int vpu_dec_parse_vid_cfg(struct nx_vpu_ctx *ctx)
 int vpu_dec_init(struct nx_vpu_ctx *ctx)
 {
 	struct vpu_dec_ctx *dec_ctx = &ctx->codec.dec;
-	struct vpu_dec_reg_frame_arg *frameArg = dec_ctx->frameArg;
+	struct vpu_dec_reg_frame_arg *frameArg;
 	int i, ret = 0;
 
 	FUNC_IN();
@@ -954,6 +954,10 @@ int vpu_dec_init(struct nx_vpu_ctx *ctx)
 		NX_ErrMsg(("Err : vpu is not opend\n"));
 		return -EAGAIN;
 	}
+	frameArg = kzalloc(sizeof(struct vpu_dec_reg_frame_arg),
+			GFP_KERNEL);
+	if (!frameArg)
+		return -ENOMEM;
 
 	frameArg->chromaInterleave = ctx->chromaInterleave;
 
@@ -1279,11 +1283,6 @@ int alloc_decoder_memory(struct nx_vpu_ctx *ctx)
 			goto Error_Exit;
 		}
 	}
-
-	dec_ctx->frameArg = kzalloc(sizeof(struct vpu_dec_reg_frame_arg),
-			GFP_KERNEL);
-	if (!dec_ctx->frameArg)
-		goto Error_Exit;
 
 	return 0;
 