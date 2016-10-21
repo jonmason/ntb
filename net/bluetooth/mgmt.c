jonmason/ntb

commit 7d86eca43fe548ba99a04d9c325fbbad95693e09

Workaround: Bluetooth: disable secure connection
This patch disables secure connection feature of marvel
bluetooth driver because the sc feature is not stable
in current kernel. We need to do more test and will be
reverted after resolve the issues.

To enable secure connection feature, enter u-boot>

setenv opts loglevel=4 sc_force_disable=0; saveenv; reset

Change-Id: I16de884ab26e4536055c211d78774bdd8e6632d6
Signed-off-by: Chanho Park <chanho61.park@samsung.com>

net/bluetooth/mgmt.c
@@ -37,6 +37,25 @@
 #include "smp.h"
 #include "mgmt_util.h"
 
+#ifdef CONFIG_MRVL_BT_DRIVER
+static bool sc_force_disable = true;
+#else
+static bool sc_force_disable;
+#endif
+
+static int sc_force_disable_setup(char *str)
+{
+	int rc = simple_strtol(str, NULL, 0);
+	if (rc)
+		sc_force_disable = true;
+	else
+		sc_force_disable = false;
+
+	return 1;
+}
+
+__setup("sc_force_disable=", sc_force_disable_setup);
+
 #define MGMT_VERSION	1
 #define MGMT_REVISION	10
 
@@ -631,14 +650,15 @@ static u32 get_supported_settings(struct hci_dev *hdev)
 			settings |= MGMT_SETTING_HS;
 		}
 
-		if (lmp_sc_capable(hdev))
+		if (lmp_sc_capable(hdev) && !sc_force_disable)
 			settings |= MGMT_SETTING_SECURE_CONN;
 	}
 
 	if (lmp_le_capable(hdev)) {
 		settings |= MGMT_SETTING_LE;
 		settings |= MGMT_SETTING_ADVERTISING;
-		settings |= MGMT_SETTING_SECURE_CONN;
+		if (!sc_force_disable)
+			settings |= MGMT_SETTING_SECURE_CONN;
 		settings |= MGMT_SETTING_PRIVACY;
 		settings |= MGMT_SETTING_STATIC_ADDRESS;
 	}

commit c8fe8206255f2a606a01192e7f7a8da1f02a5595

Bluetooth: workaround: correct disable secure connection
Fixes: 7d86eca43fe5 ("Workaround: Bluetooth: disable secure connection")

This patch corrects ifdef guard of marvel driver. The ifdef
guard is not working in case of module driver.

Change-Id: I1d444160f8b6f69e0d577ce261b699a762efdd93
Signed-off-by: Chanho Park <chanho61.park@samsung.com>

net/bluetooth/mgmt.c
@@ -37,7 +37,7 @@
 #include "smp.h"
 #include "mgmt_util.h"
 
-#ifdef CONFIG_MRVL_BT_DRIVER
+#ifdef CONFIG_MARVELL_DRIVERS
 static bool sc_force_disable = true;
 #else
 static bool sc_force_disable;