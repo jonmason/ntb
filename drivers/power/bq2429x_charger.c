jonmason/ntb

commit 46539a80f35f56dec69bef764851805326d8de2b

power: bq24296_charger: disable usb_detect_work
This patch changes the condition of running usb_detect_work.
usb_detect_work is only valid with dc detection pin and usb charging.

Change-Id: I44131e6e6cd1c490ae05d2da9d03d00c30176c83
Signed-off-by: Dongil Park <dongil01.park@samsung.com>

drivers/power/bq2429x_charger.c
@@ -562,6 +562,13 @@ static int bq24296_battery_probe(struct i2c_client *client,
 	mutex_init(&di->var_lock);
 	INIT_DELAYED_WORK(&di->usb_detect_work, usb_detect_work_func);
 	schedule_delayed_work(&di->usb_detect_work, 0);
+
+	/* usb_detect_work is valid when we support usb changing and dc_det_pin */
+	if (gpio_is_valid(bq24296_pdata->dc_det_pin)) {
+		INIT_DELAYED_WORK(&di->usb_detect_work, usb_detect_work_func);
+		schedule_delayed_work(&di->usb_detect_work, 0);
+	}
+
 	bq24296_init_registers();
 
 	if (gpio_is_valid(pdev->chg_irq_pin)) {

commit 82fb7f9a6f3a92ffc879029f850a938e587d9d28

power: bq24296_charger: Remove usb detect workqueue.
This patch removes remaining usb detect workqueue which already fixed
by below commit. usb detect workqueue should be checked the supporting
detection functionality.

Fixes: 46539a80f35f ("power: bq24296_charger: disable usb_detect_work")

	commit 93dc91a8010b3ca002e402c3bf917f96db21b49f
	Author: Dongil Park <dongil01.park@samsung.com>
	Date:   Tue Oct 18 12:03:18 2016 +0900

    	power: bq24296_charger: set current limit for dc charging

Change-Id: I0b8a0fd8b69ad0d5b43aed0d91aaaf794bda9fa9
Signed-off-by: Dongil Park <dongil01.park@samsung.com>

drivers/power/bq2429x_charger.c
@@ -560,8 +560,6 @@ static int bq24296_battery_probe(struct i2c_client *client,
 	di->workqueue = create_singlethread_workqueue("bq24296_irq");
 	INIT_WORK(&di->irq_work, irq_work_func);
 	mutex_init(&di->var_lock);
-	INIT_DELAYED_WORK(&di->usb_detect_work, usb_detect_work_func);
-	schedule_delayed_work(&di->usb_detect_work, 0);
 
 	/* usb_detect_work is valid when we support usb changing and dc_det_pin */
 	if (gpio_is_valid(bq24296_pdata->dc_det_pin)) {