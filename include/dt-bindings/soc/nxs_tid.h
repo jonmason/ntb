7b3c8782739b6d819ece4c459e8b7a03de532214
Sungwoo Park <swpark@nexell.co.kr>
2016-11-18 17:03:00 +0900

    arm64: dts: add defines of Nexell Stream BUS TID
    TID is Target ID of primitive IP in Nexell Stream BUS.
    Source primitive IP must set TID when making connection.
    
    Change-Id: I2761aa908b327a510e2c1dd9db4c9471b696dd1a
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

---
+++ include/dt-bindings/soc/nxs_tid.h
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2016  Nexell Co., Ltd.
+ * Author: Sungwoo, Park <swpark@nexell.co.kr>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _DT_BINDINGS_NXS_TID_H
+#define _DT_BINDINGS_NXS_TID_H
+
+#define NXS_TID_MIPI_DSI	50
+#define NXS_TID_CSC0		10
+#define NXS_TID_CSC1		11
+#define NXS_TID_CSC2		12
+#define NXS_TID_CSC3		13
+#define NXS_TID_LVDS0		48
+#define NXS_TID_LVDS1		49
+#define NXS_TID_ISP		0
+#define NXS_TID_GAMMA0		32
+#define NXS_TID_GAMMA1		33
+#define NXS_TID_DPC0		34
+#define NXS_TID_DPC1		35
+#define NXS_TID_BLENDER0_SCREEN	14
+#define NXS_TID_BLENDER0_IMG	15
+#define NXS_TID_BLENDER1_SCREEN	16
+#define NXS_TID_BLENDER1_IMG	17
+#define NXS_TID_BLENDER2_SCREEN	18
+#define NXS_TID_BLENDER2_IMG	19
+#define NXS_TID_BLENDER3_SCREEN	20
+#define NXS_TID_BLENDER3_IMG	21
+#define NXS_TID_BLENDER4_SCREEN	22
+#define NXS_TID_BLENDER4_IMG	23
+#define NXS_TID_BLENDER5_SCREEN	24
+#define NXS_TID_BLENDER5_IMG	25
+#define NXS_TID_BLENDER6_SCREEN	26
+#define NXS_TID_BLENDER6_IMG	27
+#define NXS_TID_BLENDER7_SCREEN	28
+#define NXS_TID_BLENDER7_IMG	29
+#define NXS_TID_HUE0		30
+#define NXS_TID_HUE1		31
+#define NXS_TID_FIFO0		52
+#define NXS_TID_FIFO1		53
+#define NXS_TID_FIFO2		54
+#define NXS_TID_FIFO3		55
+#define NXS_TID_FIFO4		56
+#define NXS_TID_FIFO5		57
+#define NXS_TID_FIFO6		58
+#define NXS_TID_FIFO7		59
+#define NXS_TID_FIFO8		60
+#define NXS_TID_FIFO9		61
+#define NXS_TID_FIFO10		62
+#define NXS_TID_FIFO11		63
+#define NXS_TID_HDMI		51
+#define NXS_TID_DMAW0		36
+#define NXS_TID_DMAW1		37
+#define NXS_TID_DMAW2		38
+#define NXS_TID_DMAW3		39
+#define NXS_TID_DMAW4		40
+#define NXS_TID_DMAW5		41
+#define NXS_TID_DMAW6		42
+#define NXS_TID_DMAW7		43
+#define NXS_TID_DMAW8		44
+#define NXS_TID_DMAW9		45
+#define NXS_TID_DMAW10		46
+#define NXS_TID_DMAW11		47
+#define NXS_TID_MULTITAP0	3
+#define NXS_TID_MULTITAP1	4
+#define NXS_TID_MULTITAP2	5
+#define NXS_TID_MULTITAP3	6
+#define NXS_TID_CROP0		1
+#define NXS_TID_CROP1		2
+#define NXS_TID_SCALER0		7
+#define NXS_TID_SCALER1		8
+#define NXS_TID_SCALER2		9
+#define NXS_TID_DEFAULT		127
+
+#endif
