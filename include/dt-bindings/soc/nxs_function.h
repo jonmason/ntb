c396fff097763ebbfdf797ace15b9ab33bcd5085
Sungwoo Park <swpark@nexell.co.kr>
2016-11-18 17:00:23 +0900

    arm64: dts: add Nexell Stream BUS function defines
    This patch adds Nexell Stream BUS function defines for dt-bindings of
    Nexell Stream BUS. SoCs using Nexell Stream BUS can use this defines.
    
    Change-Id: I2e45729bd6016bd19a8eb84e85ecebae4a33f3da
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

---
+++ include/dt-bindings/soc/nxs_function.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2016  Nexell Co., Ltd.
+ * Author: Sungwoo, Park <swpark@nexell.co.kr>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _DT_BINDINGS_NXS_FUNCTION_H
+#define _DT_BINDINGS_NXS_FUNCTION_H
+
+#define	NXS_FUNCTION_NONE		0
+#define	NXS_FUNCTION_DMAR		1
+#define	NXS_FUNCTION_DMAW		2
+#define	NXS_FUNCTION_VIP_CLIPPER	3
+#define	NXS_FUNCTION_VIP_DECIMATOR	4
+#define	NXS_FUNCTION_MIPI_CSI		5
+#define	NXS_FUNCTION_ISP2DISP		6
+#define	NXS_FUNCTION_CROPPER		7
+#define	NXS_FUNCTION_MULTITAP		8
+#define	NXS_FUNCTION_SCALER_4096	9
+#define	NXS_FUNCTION_SCALER_5376	10
+#define	NXS_FUNCTION_MLC_BOTTOM		11
+#define	NXS_FUNCTION_MLC_BLENDER	12
+#define	NXS_FUNCTION_HUE		13
+#define	NXS_FUNCTION_GAMMA		14
+#define	NXS_FUNCTION_FIFO		15
+#define	NXS_FUNCTION_MAPCONV		16
+#define	NXS_FUNCTION_TPGEN		17
+#define	NXS_FUNCTION_DISP2ISP		18
+#define	NXS_FUNCTION_CSC		19
+#define	NXS_FUNCTION_DPC		20
+#define	NXS_FUNCTION_LVDS		21
+#define	NXS_FUNCTION_MIPI_DSI		22
+#define	NXS_FUNCTION_HDMI		23
+#define	NXS_FUNCTION_MAX		24
+
+#endif
