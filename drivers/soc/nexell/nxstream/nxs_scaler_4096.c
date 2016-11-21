4624e3cfb139d3b20567950f9ba656dc65d2d23b
Sungwoo Park <swpark@nexell.co.kr>
2016-11-21 12:53:54 +0900

    soc: nexell: Introduce nxs_scaler_4096 driver
    This patch introduces nxs_scaler_4096 driver.
    nxs_scaler_4096 is primitive nxs_dev driver for Nexell Stream BUS SCALER
    IP that has 4096 bytes line buffer.
    
    Currently real working code is not implemented.
    
    Change-Id: Ic5eb563658a4591989c355c5ed5347faca4a4dec
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

---
+++ drivers/soc/nexell/nxstream/nxs_scaler_4096.c
@@ -0,0 +1,164 @@
+/*
+ * Copyright (C) 2016  Nexell Co., Ltd.
+ * Author: Sungwoo, Park <swpark@nexell.co.kr>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+#include <linux/of.h>
+#include <linux/clk.h>
+#include <linux/reset.h>
+#include <linux/interrupt.h>
+#include <linux/platform_device.h>
+
+#include <linux/soc/nexell/nxs_function.h>
+#include <linux/soc/nexell/nxs_dev.h>
+#include <linux/soc/nexell/nxs_res_manager.h>
+
+static void scaler_4096_set_interrupt_enable(const struct nxs_dev *pthis, int type,
+				     bool enable)
+{
+}
+
+static u32 scaler_4096_get_interrupt_enable(const struct nxs_dev *pthis, int type)
+{
+	return 0;
+}
+
+static u32 scaler_4096_get_interrupt_pending(const struct nxs_dev *pthis, int type)
+{
+	return 0;
+}
+
+static void scaler_4096_clear_interrupt_pending(const struct nxs_dev *pthis,
+					     int type)
+{
+}
+
+static int scaler_4096_open(const struct nxs_dev *pthis)
+{
+	return 0;
+}
+
+static int scaler_4096_close(const struct nxs_dev *pthis)
+{
+	return 0;
+}
+
+static int scaler_4096_start(const struct nxs_dev *pthis)
+{
+	return 0;
+}
+
+static int scaler_4096_stop(const struct nxs_dev *pthis)
+{
+	return 0;
+}
+
+static int scaler_4096_set_syncinfo(const struct nxs_dev *pthis,
+			    const union nxs_control *pparam)
+{
+	return 0;
+}
+
+static int scaler_4096_get_syncinfo(const struct nxs_dev *pthis,
+			    union nxs_control *pparam)
+{
+	return 0;
+}
+
+static int nxs_scaler_4096_probe(struct platform_device *pdev)
+{
+	int ret;
+	struct nxs_dev *nxs_dev;
+
+	nxs_dev = devm_kzalloc(&pdev->dev, sizeof(*nxs_dev), GFP_KERNEL);
+	if (!nxs_dev)
+		return -ENOMEM;
+
+
+	ret = nxs_dev_parse_dt(pdev, nxs_dev);
+	if (ret)
+		return ret;
+
+	nxs_dev->set_interrupt_enable = scaler_4096_set_interrupt_enable;
+	nxs_dev->get_interrupt_enable = scaler_4096_get_interrupt_enable;
+	nxs_dev->get_interrupt_pending = scaler_4096_get_interrupt_pending;
+	nxs_dev->clear_interrupt_pending = scaler_4096_clear_interrupt_pending;
+	nxs_dev->open = scaler_4096_open;
+	nxs_dev->close = scaler_4096_close;
+	nxs_dev->start = scaler_4096_start;
+	nxs_dev->stop = scaler_4096_stop;
+	nxs_dev->set_control = nxs_set_control;
+	nxs_dev->get_control = nxs_get_control;
+	nxs_dev->dev_services[0].type = NXS_CONTROL_SYNCINFO;
+	nxs_dev->dev_services[0].set_control = scaler_4096_set_syncinfo;
+	nxs_dev->dev_services[0].get_control = scaler_4096_get_syncinfo;
+
+	nxs_dev->dev = &pdev->dev;
+
+	ret = register_nxs_dev(nxs_dev);
+	if (ret)
+		return ret;
+
+	dev_info(nxs_dev->dev, "%s: success\n", __func__);
+	platform_set_drvdata(pdev, nxs_dev);
+
+	return 0;
+}
+
+static int nxs_scaler_4096_remove(struct platform_device *pdev)
+{
+	struct nxs_dev *nxs_dev = platform_get_drvdata(pdev);
+
+	if (nxs_dev)
+		unregister_nxs_dev(nxs_dev);
+
+	return 0;
+}
+
+static const struct of_device_id nxs_scaler_4096_match[] = {
+	{ .compatible = "nexell,scaler_4096-nxs-1.0", },
+	{},
+};
+
+static struct platform_driver nxs_scaler_4096_driver = {
+	.probe	= nxs_scaler_4096_probe,
+	.remove	= nxs_scaler_4096_remove,
+	.driver	= {
+		.name = "nxs-scaler_4096",
+		.of_match_table = of_match_ptr(nxs_scaler_4096_match),
+	},
+};
+
+static int __init nxs_scaler_4096_init(void)
+{
+	return platform_driver_register(&nxs_scaler_4096_driver);
+}
+/* subsys_initcall(nxs_scaler_4096_init); */
+fs_initcall(nxs_scaler_4096_init);
+
+static void __exit nxs_scaler_4096_exit(void)
+{
+	platform_driver_unregister(&nxs_scaler_4096_driver);
+}
+module_exit(nxs_scaler_4096_exit);
+
+MODULE_DESCRIPTION("Nexell Stream SCALER_4096 driver");
+MODULE_AUTHOR("Sungwoo Park, <swpark@nexell.co.kr>");
+MODULE_LICENSE("GPL");
+
