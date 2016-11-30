c72904922f4e04f1dccbf8ee51becbb2785d77b1
Sungwoo Park <swpark@nexell.co.kr>
2016-11-30 15:14:24 +0900

    soc: nexell: nxs_hdmi: Add nxs_hdmi structure
    This patch adds nxs_hdmi structure and embeds nxs_dev to nxs_hdmi.
    nxs_dev is common structure so, extending of private feature is limited.
    So, nxs_hdmi structure will have features and attributes specific to
    nxs hdmi.
    
    Change-Id: Ia470fb3b46f022672539be0f8f51797887f1cd2c
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- drivers/soc/nexell/nxstream/nxs_hdmi.c
+++ drivers/soc/nexell/nxstream/nxs_hdmi.c
@@ -29,6 +29,10 @@
 #include <linux/soc/nexell/nxs_dev.h>
 #include <linux/soc/nexell/nxs_res_manager.h>
 
+struct nxs_hdmi {
+	struct nxs_dev nxs_dev;
+};
+
 static void hdmi_set_interrupt_enable(const struct nxs_dev *pthis, int type,
 				     bool enable)
 {
@@ -83,12 +87,14 @@ static int hdmi_get_syncinfo(const struct nxs_dev *pthis,
 static int nxs_hdmi_probe(struct platform_device *pdev)
 {
 	int ret;
+	struct nxs_hdmi *hdmi;
 	struct nxs_dev *nxs_dev;
 
-	nxs_dev = devm_kzalloc(&pdev->dev, sizeof(*nxs_dev), GFP_KERNEL);
-	if (!nxs_dev)
+	hdmi = devm_kzalloc(&pdev->dev, sizeof(*hdmi), GFP_KERNEL);
+	if (!hdmi)
 		return -ENOMEM;
 
+	nxs_dev = &hdmi->nxs_dev;
 
 	ret = nxs_dev_parse_dt(pdev, nxs_dev);
 	if (ret)
@@ -115,17 +121,17 @@ static int nxs_hdmi_probe(struct platform_device *pdev)
 		return ret;
 
 	dev_info(nxs_dev->dev, "%s: success\n", __func__);
-	platform_set_drvdata(pdev, nxs_dev);
+	platform_set_drvdata(pdev, hdmi);
 
 	return 0;
 }
 
 static int nxs_hdmi_remove(struct platform_device *pdev)
 {
-	struct nxs_dev *nxs_dev = platform_get_drvdata(pdev);
+	struct nxs_hdmi *hdmi = platform_get_drvdata(pdev);
 
-	if (nxs_dev)
-		unregister_nxs_dev(nxs_dev);
+	if (hdmi)
+		unregister_nxs_dev(&hdmi->nxs_dev);
 
 	return 0;
 }
