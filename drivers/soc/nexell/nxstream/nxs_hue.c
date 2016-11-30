72fb0edfefbfd5f651f300c447ced5f494dfddb4
Sungwoo Park <swpark@nexell.co.kr>
2016-11-30 15:18:01 +0900

    soc: nexell: nxs_hue: Add nxs_hue structure
    This patch adds nxs_hue structure and embeds nxs_dev to nxs_hue.
    nxs_dev is common structure so, extending of private feature is limited.
    So, nxs_hue structure will have features and attributes specific to
    nxs hue.
    
    Change-Id: I0384500eb9d6ad153f84fee81d608d626f758df7
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- drivers/soc/nexell/nxstream/nxs_hue.c
+++ drivers/soc/nexell/nxstream/nxs_hue.c
@@ -29,6 +29,10 @@
 #include <linux/soc/nexell/nxs_dev.h>
 #include <linux/soc/nexell/nxs_res_manager.h>
 
+struct nxs_hue {
+	struct nxs_dev nxs_dev;
+};
+
 static void hue_set_interrupt_enable(const struct nxs_dev *pthis, int type,
 				     bool enable)
 {
@@ -83,12 +87,14 @@ static int hue_get_syncinfo(const struct nxs_dev *pthis,
 static int nxs_hue_probe(struct platform_device *pdev)
 {
 	int ret;
+	struct nxs_hue *hue;
 	struct nxs_dev *nxs_dev;
 
-	nxs_dev = devm_kzalloc(&pdev->dev, sizeof(*nxs_dev), GFP_KERNEL);
-	if (!nxs_dev)
+	hue = devm_kzalloc(&pdev->dev, sizeof(*hue), GFP_KERNEL);
+	if (!hue)
 		return -ENOMEM;
 
+	nxs_dev = &hue->nxs_dev;
 
 	ret = nxs_dev_parse_dt(pdev, nxs_dev);
 	if (ret)
@@ -115,17 +121,17 @@ static int nxs_hue_probe(struct platform_device *pdev)
 		return ret;
 
 	dev_info(nxs_dev->dev, "%s: success\n", __func__);
-	platform_set_drvdata(pdev, nxs_dev);
+	platform_set_drvdata(pdev, hue);
 
 	return 0;
 }
 
 static int nxs_hue_remove(struct platform_device *pdev)
 {
-	struct nxs_dev *nxs_dev = platform_get_drvdata(pdev);
+	struct nxs_hue *hue = platform_get_drvdata(pdev);
 
-	if (nxs_dev)
-		unregister_nxs_dev(nxs_dev);
+	if (hue)
+		unregister_nxs_dev(&hue->nxs_dev);
 
 	return 0;
 }
