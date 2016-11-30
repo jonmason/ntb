411bfbb047698032eb54614fd9e26d1b945b50ff
Sungwoo Park <swpark@nexell.co.kr>
2016-11-30 14:16:11 +0900

    soc: nexell: nxs_cropper: Add nxs_cropper structure
    This patch adds nxs_cropper structure and embeds nxs_dev to nxs_cropper.
    nxs_dev is common structure so, extending of private feature is limited.
    So, nxs_cropper structure will have features and attributes specific to
    nxs cropper.
    
    Change-Id: I31b53a3180563d17c2854c3e327d4ab97ce16c23
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- drivers/soc/nexell/nxstream/nxs_cropper.c
+++ drivers/soc/nexell/nxstream/nxs_cropper.c
@@ -29,6 +29,10 @@
 #include <linux/soc/nexell/nxs_dev.h>
 #include <linux/soc/nexell/nxs_res_manager.h>
 
+struct nxs_cropper {
+	struct nxs_dev nxs_dev;
+};
+
 static void cropper_set_interrupt_enable(const struct nxs_dev *pthis, int type,
 				     bool enable)
 {
@@ -84,12 +88,14 @@ static int cropper_get_syncinfo(const struct nxs_dev *pthis,
 static int nxs_cropper_probe(struct platform_device *pdev)
 {
 	int ret;
+	struct nxs_cropper *cropper;
 	struct nxs_dev *nxs_dev;
 
-	nxs_dev = devm_kzalloc(&pdev->dev, sizeof(*nxs_dev), GFP_KERNEL);
-	if (!nxs_dev)
+	cropper = devm_kzalloc(&pdev->dev, sizeof(*cropper), GFP_KERNEL);
+	if (!cropper)
 		return -ENOMEM;
 
+	nxs_dev = &cropper->nxs_dev;
 
 	ret = nxs_dev_parse_dt(pdev, nxs_dev);
 	if (ret)
@@ -116,17 +122,17 @@ static int nxs_cropper_probe(struct platform_device *pdev)
 		return ret;
 
 	dev_info(nxs_dev->dev, "%s: success\n", __func__);
-	platform_set_drvdata(pdev, nxs_dev);
+	platform_set_drvdata(pdev, cropper);
 
 	return 0;
 }
 
 static int nxs_cropper_remove(struct platform_device *pdev)
 {
-	struct nxs_dev *nxs_dev = platform_get_drvdata(pdev);
+	struct nxs_cropper *cropper = platform_get_drvdata(pdev);
 
-	if (nxs_dev)
-		unregister_nxs_dev(nxs_dev);
+	if (cropper)
+		unregister_nxs_dev(&cropper->nxs_dev);
 
 	return 0;
 }
