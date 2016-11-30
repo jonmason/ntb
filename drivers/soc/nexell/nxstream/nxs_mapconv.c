86e6a8e77a32ca6fad23bd48a17d508da24b41aa
Sungwoo Park <swpark@nexell.co.kr>
2016-11-30 15:33:21 +0900

    soc: nexell: nxs_mapconv: Add nxs_mapconv structure
    This patch adds nxs_mapconv structure and embeds nxs_dev to
    nxs_mapconv. nxs_dev is common structure so, extending of private
    feature is limited. So, nxs_mapconv structure will have features and
    attributes specific to nxs mapconv.
    
    Change-Id: I2c7e61962f4ec1628aa5fd73a8e9e0be9268e3f8
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- drivers/soc/nexell/nxstream/nxs_mapconv.c
+++ drivers/soc/nexell/nxstream/nxs_mapconv.c
@@ -29,6 +29,10 @@
 #include <linux/soc/nexell/nxs_dev.h>
 #include <linux/soc/nexell/nxs_res_manager.h>
 
+struct nxs_mapconv {
+	struct nxs_dev nxs_dev;
+};
+
 static void mapconv_set_interrupt_enable(const struct nxs_dev *pthis, int type,
 				     bool enable)
 {
@@ -84,12 +88,14 @@ static int mapconv_get_syncinfo(const struct nxs_dev *pthis,
 static int nxs_mapconv_probe(struct platform_device *pdev)
 {
 	int ret;
+	struct nxs_mapconv *mapconv;
 	struct nxs_dev *nxs_dev;
 
-	nxs_dev = devm_kzalloc(&pdev->dev, sizeof(*nxs_dev), GFP_KERNEL);
-	if (!nxs_dev)
+	mapconv = devm_kzalloc(&pdev->dev, sizeof(*mapconv), GFP_KERNEL);
+	if (!mapconv)
 		return -ENOMEM;
 
+	nxs_dev = &mapconv->nxs_dev;
 
 	ret = nxs_dev_parse_dt(pdev, nxs_dev);
 	if (ret)
@@ -116,17 +122,17 @@ static int nxs_mapconv_probe(struct platform_device *pdev)
 		return ret;
 
 	dev_info(nxs_dev->dev, "%s: success\n", __func__);
-	platform_set_drvdata(pdev, nxs_dev);
+	platform_set_drvdata(pdev, mapconv);
 
 	return 0;
 }
 
 static int nxs_mapconv_remove(struct platform_device *pdev)
 {
-	struct nxs_dev *nxs_dev = platform_get_drvdata(pdev);
+	struct nxs_mapconv *mapconv = platform_get_drvdata(pdev);
 
-	if (nxs_dev)
-		unregister_nxs_dev(nxs_dev);
+	if (mapconv)
+		unregister_nxs_dev(&mapconv->nxs_dev);
 
 	return 0;
 }
