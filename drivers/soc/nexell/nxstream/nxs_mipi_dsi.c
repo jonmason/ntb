9418defa9d141c2fb7952e3d53fdf896a47b2c85
Sungwoo Park <swpark@nexell.co.kr>
2016-11-30 15:42:26 +0900

    soc: nexell: nxs_mipi_dsi: Add nxs_dsi structure
    This patch adds nxs_dsi structure and embeds nxs_dev to nxs_dsi.
    nxs_dev is common structure so, extending of private feature is limited.
    So, nxs_dsi structure will have features and attributes specific to
    nxs mipi dsi.
    
    Change-Id: Ia70820d6838c021b7a4a74c14c4633d98340be62
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- drivers/soc/nexell/nxstream/nxs_mipi_dsi.c
+++ drivers/soc/nexell/nxstream/nxs_mipi_dsi.c
@@ -29,6 +29,10 @@
 #include <linux/soc/nexell/nxs_dev.h>
 #include <linux/soc/nexell/nxs_res_manager.h>
 
+struct nxs_dsi {
+	struct nxs_dev nxs_dev;
+};
+
 static void mipi_dsi_set_interrupt_enable(const struct nxs_dev *pthis, int type,
 				     bool enable)
 {
@@ -84,12 +88,14 @@ static int mipi_dsi_get_syncinfo(const struct nxs_dev *pthis,
 static int nxs_mipi_dsi_probe(struct platform_device *pdev)
 {
 	int ret;
+	struct nxs_dsi *dsi;
 	struct nxs_dev *nxs_dev;
 
-	nxs_dev = devm_kzalloc(&pdev->dev, sizeof(*nxs_dev), GFP_KERNEL);
-	if (!nxs_dev)
+	dsi = devm_kzalloc(&pdev->dev, sizeof(*dsi), GFP_KERNEL);
+	if (!dsi)
 		return -ENOMEM;
 
+	nxs_dev = &dsi->nxs_dev;
 
 	ret = nxs_dev_parse_dt(pdev, nxs_dev);
 	if (ret)
@@ -116,17 +122,17 @@ static int nxs_mipi_dsi_probe(struct platform_device *pdev)
 		return ret;
 
 	dev_info(nxs_dev->dev, "%s: success\n", __func__);
-	platform_set_drvdata(pdev, nxs_dev);
+	platform_set_drvdata(pdev, dsi);
 
 	return 0;
 }
 
 static int nxs_mipi_dsi_remove(struct platform_device *pdev)
 {
-	struct nxs_dev *nxs_dev = platform_get_drvdata(pdev);
+	struct nxs_dsi *dsi = platform_get_drvdata(pdev);
 
-	if (nxs_dev)
-		unregister_nxs_dev(nxs_dev);
+	if (dsi)
+		unregister_nxs_dev(&dsi->nxs_dev);
 
 	return 0;
 }
