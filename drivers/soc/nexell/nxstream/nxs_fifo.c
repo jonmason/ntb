2b1a13773e68cab33445edfabf817aceef936232
Sungwoo Park <swpark@nexell.co.kr>
2016-12-06 16:49:42 +0900

    soc: nexell: nxs_fifo: Implement set_dirty, set_tid
    This patch implements set_dirty, set_tid function and handles register
    base address by syscon regmap interface.
    
    Change-Id: Ie5b4ddb9f3ba857395b119bead3caa2b9dbf4a12
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- drivers/soc/nexell/nxstream/nxs_fifo.c
+++ drivers/soc/nexell/nxstream/nxs_fifo.c
@@ -22,16 +22,59 @@
 #include <linux/of.h>
 #include <linux/clk.h>
 #include <linux/reset.h>
+#include <linux/regmap.h>
 #include <linux/interrupt.h>
+#include <linux/mfd/syscon.h>
 #include <linux/platform_device.h>
 
 #include <linux/soc/nexell/nxs_function.h>
 #include <linux/soc/nexell/nxs_dev.h>
 #include <linux/soc/nexell/nxs_res_manager.h>
 
+#define FIFO_CTRL		0x0000
+#define FIFO_INT		0x0004
+
+/* FIFO CTRL */
+#define FIFO_REG_CLEAR_SHIFT	21
+#define FIFO_REG_CLEAR_MASK	BIT(21)
+#define FIFO_DIRTY_SHIFT	20
+#define FIFO_DIRTY_MASK		BIT(20)
+#define FIFO_DIRTY_CLR_SHIFT	19
+#define FIFO_DIRTY_CLR_MASK	BIT(19)
+#define FIFO_TZPROT_SHIFT	18
+#define FIFO_TZPROT_MASK	BIT(18)
+#define FIFO_BUFFERED_ENABLE_SHIFT	17
+#define FIFO_BUFFERED_ENABLE_MASK	BIT(17)
+#define FIFO_ENABLE_SHIFT	16
+#define FIFO_ENABLE_MASK	BIT(16)
+#define FIFO_IDLE_SHIFT		15
+#define FIFO_IDLE_MASK		BIT(15)
+#define FIFO_INIT_SHIFT		14
+#define FIFO_INIT_MASK		BIT(14)
+#define FIFO_TID_SHIFT		0
+#define FIFO_TID_MASK		GENMASK(13, 0)
+
+/* FIFO INT */
+#define FIFO_IDLE_INTDISABLE_SHIFT	6
+#define FIFO_IDLE_INTDISABLE_MASK	BIT(6)
+#define FIFO_IDLE_INTENB_SHIFT		5
+#define FIFO_IDLE_INTENB_MASK		BIT(5)
+#define FIFO_IDLE_INTPEND_CLR_SHIFT	4
+#define FIFO_IDLE_INTPEND_CLR_MASK	BIT(4)
+#define FIFO_UPDATE_INTDISABLE_SHIFT	2
+#define FIFO_UPDATE_INTDISABLE_MASK	BIT(2)
+#define FIFO_UPDATE_INTENB_SHIFT	1
+#define FIFO_UPDATE_INTENB_MASK		BIT(1)
+#define FIFO_UPDATE_INTPEND_CLR_SHIFT	0
+#define FIFO_UPDATE_INTPEND_CLR_MASK	BIT(0)
+
 struct nxs_fifo {
 	struct nxs_dev nxs_dev;
+	struct regmap *reg;
+	u32 offset;
 };
+
+#define nxs_to_fifo(dev)	container_of(dev, struct nxs_fifo, nxs_dev)
 
 static void fifo_set_interrupt_enable(const struct nxs_dev *pthis, int type,
 				     bool enable)
@@ -74,12 +117,20 @@ static int fifo_stop(const struct nxs_dev *pthis)
 
 static int fifo_set_dirty(const struct nxs_dev *pthis)
 {
-	return 0;
+	struct nxs_fifo *fifo = nxs_to_fifo(pthis);
+
+	return regmap_update_bits(fifo->reg, fifo->offset + FIFO_CTRL,
+				  FIFO_DIRTY_MASK,
+				  1 << FIFO_DIRTY_SHIFT);
 }
 
 static int fifo_set_tid(const struct nxs_dev *pthis, u32 tid1, u32 tid2)
 {
-	return 0;
+	struct nxs_fifo *fifo = nxs_to_fifo(pthis);
+
+	return regmap_update_bits(fifo->reg, fifo->offset + FIFO_CTRL,
+				  FIFO_TID_MASK,
+				  tid1 << FIFO_TID_SHIFT);
 }
 
 static int fifo_set_syncinfo(const struct nxs_dev *pthis,
@@ -99,16 +150,30 @@ static int nxs_fifo_probe(struct platform_device *pdev)
 	int ret;
 	struct nxs_fifo *fifo;
 	struct nxs_dev *nxs_dev;
+	struct resource *res;
 
 	fifo = devm_kzalloc(&pdev->dev, sizeof(*fifo), GFP_KERNEL);
 	if (!fifo)
 		return -ENOMEM;
 
 	nxs_dev = &fifo->nxs_dev;
-
 	ret = nxs_dev_parse_dt(pdev, nxs_dev);
 	if (ret)
 		return ret;
+
+	fifo->reg = syscon_regmap_lookup_by_phandle(pdev->dev.of_node,
+						    "syscon");
+	if (IS_ERR(fifo->reg)) {
+		dev_err(&pdev->dev, "unable to get syscon\n");
+		return PTR_ERR(fifo->reg);
+	}
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (!res) {
+		dev_err(&pdev->dev, "missing IO resource\n");
+		return -ENODEV;
+	}
+	fifo->offset = res->start;
 
 	nxs_dev->set_interrupt_enable = fifo_set_interrupt_enable;
 	nxs_dev->get_interrupt_enable = fifo_get_interrupt_enable;
