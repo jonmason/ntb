ce399a17c430be1fa7e3d895ca29791f325c7641
Sungwoo Park <swpark@nexell.co.kr>
2016-12-06 16:52:39 +0900

    soc: nexell: nxs_gamma: Implement set_dirty, set_tid
    This patch implements set_dirty, set_tid function and handles register
    base address by syscon regmap interface.
    
    Change-Id: I0121a1f275e8534485e4010dd743b79aa152d4da
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- drivers/soc/nexell/nxstream/nxs_gamma.c
+++ drivers/soc/nexell/nxstream/nxs_gamma.c
@@ -22,16 +22,60 @@
 #include <linux/of.h>
 #include <linux/clk.h>
 #include <linux/reset.h>
+#include <linux/regmap.h>
 #include <linux/interrupt.h>
+#include <linux/mfd/syscon.h>
 #include <linux/platform_device.h>
 
 #include <linux/soc/nexell/nxs_function.h>
 #include <linux/soc/nexell/nxs_dev.h>
 #include <linux/soc/nexell/nxs_res_manager.h>
 
+#define GAMMA_DIRTYSET_OFFSET	0x0004
+#define GAMMA_DIRTYCLR_OFFSET	0x0014
+#define GAMMA0_DIRTY		BIT(10)
+#define GAMMA1_DIRTY		BIT(11)
+
+#define GAMMA_CTRL		0x0000
+#define GAMMA_INT		0x0004
+#define GAMMA_COEFF0		0x0008
+#define GAMMA_COEFF1		0x000c
+#define GAMMA_COEFF2		0x0010
+#define GAMMA_COEFF3		0x0014
+
+/* GAMMA CTRL */
+#define GAMMA_REG_CLEAR_SHIFT	19
+#define GAMMA_REG_CLEAR_MASK	BIT(19)
+#define GAMMA_TZPROT_SHIFT	18
+#define GAMMA_TZPROT_MASK	BIT(18)
+#define GAMMA_IDLE_SHIFT	17
+#define GAMMA_IDLE_MASK		BIT(17)
+#define GAMMA_ENABLE_SHIFT	16
+#define GAMMA_ENABLE_MASK	BIT(16)
+#define GAMMA_TID_SHIFT		0
+#define GAMMA_TID_MASK		GENMASK(13, 0)
+
+/* GAMMA INT */
+#define GAMMA_IDLE_INT_DISABLE_SHIFT	6
+#define GAMMA_IDLE_INT_DISABLE_MASK	BIT(6)
+#define GAMMA_IDLE_INTENB_SHIFT		5
+#define GAMMA_IDLE_INTENB_MASK		BIT(5)
+#define GAMMA_IDLE_INTPEND_CLR_SHIFT	4
+#define GAMMA_IDLE_INTPEND_CLR_MASK	BIT(4)
+#define GAMMA_UPDATE_INT_DISABLE_SHIFT	2
+#define GAMMA_UPDATE_INT_DISABLE_MASK	BIT(2)
+#define GAMMA_UPDATE_INTENB_SHIFT	1
+#define GAMMA_UPDATE_INTENB_MASK	BIT(1)
+#define GAMMA_UPDATE_INTPEND_CLR_SHIFT	0
+#define GAMMA_UPDATE_INTPEND_CLR_MASK	BIT(0)
+
 struct nxs_gamma {
 	struct nxs_dev nxs_dev;
+	struct regmap *reg;
+	u32 offset;
 };
+
+#define nxs_to_gamma(dev)	container_of(dev, struct nxs_gamma, nxs_dev)
 
 static void gamma_set_interrupt_enable(const struct nxs_dev *pthis, int type,
 				     bool enable)
@@ -74,12 +118,31 @@ static int gamma_stop(const struct nxs_dev *pthis)
 
 static int gamma_set_dirty(const struct nxs_dev *pthis)
 {
-	return 0;
+	struct nxs_gamma *gamma = nxs_to_gamma(pthis);
+	u32 dirty_val;
+
+	switch (pthis->dev_inst_index) {
+	case 0:
+		dirty_val = GAMMA0_DIRTY;
+		break;
+	case 1:
+		dirty_val = GAMMA1_DIRTY;
+		break;
+	default:
+		dev_err(pthis->dev, "invalid inst %d\n", pthis->dev_inst_index);
+		return -ENODEV;
+	}
+
+	return regmap_write(gamma->reg, GAMMA_DIRTYSET_OFFSET, dirty_val);
 }
 
 static int gamma_set_tid(const struct nxs_dev *pthis, u32 tid1, u32 tid2)
 {
-	return 0;
+	struct nxs_gamma *gamma = nxs_to_gamma(pthis);
+
+	return regmap_update_bits(gamma->reg, gamma->offset + GAMMA_CTRL,
+				  GAMMA_TID_MASK,
+				  tid1 << GAMMA_TID_SHIFT);
 }
 
 static int gamma_set_syncinfo(const struct nxs_dev *pthis,
@@ -99,16 +162,30 @@ static int nxs_gamma_probe(struct platform_device *pdev)
 	int ret;
 	struct nxs_gamma *gamma;
 	struct nxs_dev *nxs_dev;
+	struct resource *res;
 
 	gamma = devm_kzalloc(&pdev->dev, sizeof(*gamma), GFP_KERNEL);
 	if (!gamma)
 		return -ENOMEM;
 
 	nxs_dev = &gamma->nxs_dev;
-
 	ret = nxs_dev_parse_dt(pdev, nxs_dev);
 	if (ret)
 		return ret;
+
+	gamma->reg = syscon_regmap_lookup_by_phandle(pdev->dev.of_node,
+						     "syscon");
+	if (IS_ERR(gamma->reg)) {
+		dev_err(&pdev->dev, "unable to get syscon\n");
+		return PTR_ERR(gamma->reg);
+	}
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (!res) {
+		dev_err(&pdev->dev, "missing IO resource\n");
+		return -ENODEV;
+	}
+	gamma->offset = res->start;
 
 	nxs_dev->set_interrupt_enable = gamma_set_interrupt_enable;
 	nxs_dev->get_interrupt_enable = gamma_get_interrupt_enable;
