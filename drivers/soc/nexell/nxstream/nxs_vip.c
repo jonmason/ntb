44dcf6b39dc9f86cae3fdf6943612639fc0087ac
Sungwoo Park <swpark@nexell.co.kr>
2016-12-06 16:02:03 +0900

    soc: nexell: nxs_vip: Implement set_dirty, set_tid
    This patch implements set_dirty, set_tid function and handles base
    address mapping.
    VIP has different address space for registers. One is VIP register set
    and other is PRESCALER register set.
    VIP register set is for settings involved with camera sensor.
    VIP register set has one dirty.
    PRESCALER register set is for settings involved with nxstream bus.
    PRESCALER register set has one dirty and one regfire.
    dirty is used for synchronization setting at nextframe.
    regfire is used for synchronization of Nexell Stream BUS at nextframe.
    
    Client of nxs_vip must set dirty after setting VIP and PRESCALER
    register and set regfire to apply all changes of Nexell Stream BUS.
    
    Change-Id: I0f1372a307aeaac4af8a2a43d65c229d11b10903
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- drivers/soc/nexell/nxstream/nxs_vip.c
+++ drivers/soc/nexell/nxstream/nxs_vip.c
@@ -20,19 +20,96 @@
 #include <linux/module.h>
 
 #include <linux/of.h>
+#include <linux/io.h>
 #include <linux/clk.h>
 #include <linux/reset.h>
 #include <linux/interrupt.h>
+#include <linux/of_address.h>
 #include <linux/platform_device.h>
 
 #include <linux/soc/nexell/nxs_function.h>
 #include <linux/soc/nexell/nxs_dev.h>
 #include <linux/soc/nexell/nxs_res_manager.h>
 
+/* VIP Register */
+#define VIP_CONFIG		0x0000
+
+/* VIP CONFIG */
+#define VIP_DIRTY_SHIFT		31
+#define VIP_DIRTY_MASK		BIT(31)
+#define VIP_DIRTY_CLR_SHIFT	30
+#define VIP_DIRTY_CLR_MASK	BIT(30)
+#define VIP_TZPROT_SHIFT	29
+#define VIP_TZPROT_MASK		BIT(29)
+#define VIP_REG_CLEAR_SHIFT	28
+#define VIP_REG_CLEAR_MASK	BIT(28)
+#define VIP_MXL_SEL_ENB_SHIFT	12
+#define VIP_MXL_SEL_ENB_MASK	BIT(12)
+#define VIP_MXL_SEL_SHIFT	11
+#define VIP_MXL_SEL_MASK	BIT(11)
+#define VIP_MXL_ENB_SHIFT	10
+#define VIP_MXL_ENB_MASK	BIT(10)
+#define VIP_MXS_ENB_SHIFT	9
+#define VIP_MXS_ENB_MASK	BIT(9)
+#define VIP_EXTSYNCENB_SHIFT	8
+#define VIP_EXTSYNCENB_MASK	BIT(8)
+#define VIP_RAW_WIDTH_SHIFT	5
+#define VIP_RAW_WIDTH_MASK	GENMASK(6, 5)
+#define VIP_RAW_ALIGN_LSB_SHIFT	4
+#define VIP_RAW_ALIGN_LSB_MASK	BIT(4)
+#define VIP_YUYVSEL_SHIFT	2
+#define VIP_YUYVSEL_MASK	GENMASK(3, 2)
+#define VIP_WIDTH8_SHIFT	1
+#define VIP_WIDTH8_MASK		BIT(1)
+#define VIP_ENB_SHIFT		0
+#define VIP_ENB_MASK		BIT(0)
+
+/* VIP PRESCALER Register */
+#define PRESCALER_CDENB		0x0000
+#define PRESCALER_DECIMATOR_TID	0x004c
+#define PRESCALER_CLIPPER_TID	0x00a8
+
+/* VIP PRE CDENB */
+#define	PRESCALER_REG_CLEAR_SHIFT	31
+#define PRESCALER_REG_CLEAR_MASK	BIT(31)
+#define PRESCALER_TZPROT_SHIFT		12
+#define PRESCALER_TZPROT_MASK		BIT(12)
+#define PRESCALER_DIRTY_CLR_SHIFT	11
+#define PRESCALER_DIRTY_CLR_MASK	BIT(11)
+#define PRESCALER_DIRTY_SHIFT		10
+#define PRESCALER_DIRTY_MASK		BIT(10)
+#define PRESCALER_REGFIRE_SHIFT		9
+#define PRESCALER_REGFIRE_MASK		BIT(9)
+#define PRESCALER_TOP_EN_SHIFT		8
+#define PRESCALER_TOP_EN_MASK		BIT(8)
+#define PRESCALER_SIPENB_SHIFT		5
+#define PRESCALER_SIPENB_MASK		BIT(5)
+#define PRESCALER_FORMAT_SHIFT		4
+#define PRESCALER_FORMAT_MASK		BIT(4)
+#define PRESCALER_RAW_SEP_SHIFT		3
+#define PRESCALER_RAW_SEP_MASK		BIT(3)
+#define PRESCALER_FIELD_INV_SHIFT	2
+#define PRESCALER_FIELD_INV_MASK	BIT(2)
+#define PRESCALER_CLIPPER_EN_SHIFT	1
+#define PRESCALER_CLIPPER_EN_MASK	BIT(1)
+#define PRESCALER_DECIMATOR_EN_SHIFT	0
+#define PRESCALER_DECIMATOR_EN_MASK	BIT(0)
+
+/* TID */
+#define PRESCALER_TID_SHIFT		0
+#define PRESCALER_TID_MASK		GENMASK(13, 0)
+
 struct nxs_vip {
 	struct nxs_dev nxs_dev_clipper;
 	struct nxs_dev nxs_dev_decimator;
+	u8 *base; /* vip register */
+	u8 *prescaler_base; /* prescaler register */
 };
+
+#define clipper_to_vip(dev)	container_of(dev, struct nxs_vip, \
+					     nxs_dev_clipper)
+#define decimator_to_vip(dev)	container_of(dev, struct nxs_vip, \
+					     nxs_dev_decimator)
 
 static void vip_set_interrupt_enable(const struct nxs_dev *pthis, int type,
 				     bool enable)
@@ -75,11 +152,53 @@ static int vip_stop(const struct nxs_dev *pthis)
 
 static int vip_set_dirty(const struct nxs_dev *pthis)
 {
+	struct nxs_vip *vip;
+	u32 val;
+	u8 *reg;
+
+	if (pthis->dev_function == NXS_FUNCTION_VIP_CLIPPER)
+		vip = clipper_to_vip(pthis);
+	else
+		vip = decimator_to_vip(pthis);
+
+	/* VIP DIRTY */
+	reg = vip->base + VIP_CONFIG;
+	val = readl(reg);
+	val &= ~VIP_DIRTY_MASK;
+	val |= 1 << VIP_DIRTY_SHIFT;
+	writel(val, reg);
+
+	/* PRESCALER DIRTY & REG FIRE */
+	reg = vip->prescaler_base + PRESCALER_CDENB;
+	val = readl(reg);
+	val &= ~PRESCALER_DIRTY_MASK;
+	val |= 1 << PRESCALER_DIRTY_SHIFT;
+	val &= ~PRESCALER_REGFIRE_MASK;
+	val |= 1 << PRESCALER_REGFIRE_SHIFT;
+	writel(val, reg);
+
 	return 0;
 }
 
 static int vip_set_tid(const struct nxs_dev *pthis, u32 tid1, u32 tid2)
 {
+	struct nxs_vip *vip;
+	u32 val;
+	u8 *reg;
+
+	if (pthis->dev_function == NXS_FUNCTION_VIP_CLIPPER) {
+		vip = clipper_to_vip(pthis);
+		reg = vip->prescaler_base + PRESCALER_CLIPPER_TID;
+	} else {
+		vip = decimator_to_vip(pthis);
+		reg = vip->prescaler_base + PRESCALER_DECIMATOR_TID;
+	}
+
+	val = readl(reg);
+	val &= ~PRESCALER_TID_MASK;
+	val |= tid1 << PRESCALER_TID_SHIFT;
+	writel(val, reg);
+
 	return 0;
 }
 
@@ -101,16 +220,55 @@ static int nxs_vip_probe(struct platform_device *pdev)
 	struct nxs_vip *vip;
 	struct nxs_dev *nxs_dev_clipper;
 	struct nxs_dev *nxs_dev_decimator;
+	struct resource res;
 
 	vip = devm_kzalloc(&pdev->dev, sizeof(*vip), GFP_KERNEL);
 	if (!vip)
 		return -ENOMEM;
 
 	nxs_dev_clipper = &vip->nxs_dev_clipper;
-
 	ret = nxs_dev_parse_dt(pdev, nxs_dev_clipper);
 	if (ret)
 		return ret;
+
+	ret = of_address_to_resource(pdev->dev.of_node, 0, &res);
+	if (ret) {
+		dev_err(&pdev->dev,
+			"[%s:%d] failed to get vip base address\n",
+			nxs_function_to_str(nxs_dev_clipper->dev_function),
+			nxs_dev_clipper->dev_inst_index);
+		return -ENXIO;
+	}
+
+	vip->base = devm_ioremap_nocache(nxs_dev_clipper->dev, res.start,
+					 resource_size(&res));
+	if (!vip->base) {
+		dev_err(&pdev->dev,
+			"[%s:%d] failed to ioremap for vip\n",
+			nxs_function_to_str(nxs_dev_clipper->dev_function),
+			nxs_dev_clipper->dev_inst_index);
+			return -EBUSY;
+	}
+
+	ret = of_address_to_resource(pdev->dev.of_node, 1, &res);
+	if (ret) {
+		dev_err(&pdev->dev,
+			"[%s:%d] failed to get vip prescaler base address\n",
+			nxs_function_to_str(nxs_dev_clipper->dev_function),
+			nxs_dev_clipper->dev_inst_index);
+		return -ENXIO;
+	}
+
+	vip->prescaler_base = devm_ioremap_nocache(nxs_dev_clipper->dev,
+						   res.start,
+						   resource_size(&res));
+	if (!vip->prescaler_base) {
+		dev_err(&pdev->dev,
+			"[%s:%d] failed to ioremap for vip prescaler\n",
+			nxs_function_to_str(nxs_dev_clipper->dev_function),
+			nxs_dev_clipper->dev_inst_index);
+			return -EBUSY;
+	}
 
 	INIT_LIST_HEAD(&nxs_dev_clipper->list);
 	nxs_dev_clipper->set_interrupt_enable = vip_set_interrupt_enable;
@@ -141,7 +299,6 @@ static int nxs_vip_probe(struct platform_device *pdev)
 	nxs_dev_decimator->dev_ver = nxs_dev_clipper->dev_ver;
 	nxs_dev_decimator->dev_function = NXS_FUNCTION_VIP_DECIMATOR;
 	nxs_dev_decimator->dev_inst_index = nxs_dev_clipper->dev_inst_index;
-	nxs_dev_decimator->base = nxs_dev_clipper->base;
 	nxs_dev_decimator->irq = nxs_dev_clipper->irq;
 	nxs_dev_decimator->resets = nxs_dev_clipper->resets;
 	nxs_dev_decimator->reset_num = nxs_dev_clipper->reset_num;
