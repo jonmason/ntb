d5b0c989b16eb9ad23419156b921ba1e795ba16b
Youngbok Park <ybpark@nexell.co.kr>
2017-04-26 14:38:42 +0900

    cpufreq: nexell: add support dvfs for new nxp4330
    This patch add support dvfs for new nxp4330.
    
    Change-Id: Ifeb5bd98b9310756e038475f10c61f117d601bc1
    Signed-off-by: Youngbok Park <ybpark@nexell.co.kr>

--- drivers/cpufreq/s5p4418-cpufreq.h
+++ drivers/cpufreq/s5p4418-cpufreq.h
@@ -23,6 +23,7 @@ extern void nxp_cpu_id_ecid(u32 ecid[4]);
 extern void nxp_cpu_id_string(u32 string[12]);
 
 #define	CPU_ID_S5P4418		(0xE4418000)
+#define	CPU_ID_NXP4330_NEW	(0x0)
 #define	VOLTAGE_STEP_UV		(12500)	/* 12.5 mV */
 #define	ASV_DEFAULT_LEVEL	(0)		/* for 4330 */
 
@@ -127,7 +128,8 @@ static int s5p4418_asv_setup_table(unsigned long (*freq_tables)[2])
 	nxp_cpu_id_string(string);
 	nxp_cpu_id_ecid(ecid);
 
-	if (string[0] != CPU_ID_S5P4418) {
+	if ((string[0] != CPU_ID_S5P4418) &&
+			(string[0] != CPU_ID_NXP4330_NEW)) {
 		asvlv = ASV_DEFAULT_LEVEL;
 		goto asv_find;
 	}
